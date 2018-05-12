c8940a68c8ec60835502c270f7a18cf9cdbfa142
Dolan Miu <dolan_miu@hotmail.com>
2018-05-12 19:31:00 +0100

    Make minor changes

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,11 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
+#define MOTOR_COUNT 4
+
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
-// Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
-// Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
-// Adafruit_DCMotor *motor3 = AFMS.getMotor(3);
-// Adafruit_DCMotor *motor4 = AFMS.getMotor(4);
-Adafruit_DCMotor motors[4] = {
+Adafruit_DCMotor motors[MOTOR_COUNT] = {
  *AFMS.getMotor(1),
  *AFMS.getMotor(2),
  *AFMS.getMotor(3),
@@ -21,7 +19,8 @@ void setup() {
   AFMS.begin();
 
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  for(int i = 0; i < sizeof(motors); i++) {
+  for(int i = 0; i < MOTOR_COUNT; i++) {
+    Serial.println("Init Motor: " + String(i));
     motors[i].setSpeed(150);
     motors[i].run(FORWARD);
     motors[i].run(RELEASE);
@@ -30,15 +29,17 @@ void setup() {
 }
 
 void loop() {
-  for(int i = 0; i < sizeof(motors); i++) {
+  Serial.println("Running Motors");
+  for(int i = 0; i < MOTOR_COUNT; i++) {
     motors[i].run(FORWARD);
   }
   
   delay(1000);
-  
-  for(int i = 0; i < sizeof(motors); i++) {
+
+  Serial.println("Stopping Motors");
+  for(int i = 0; i < MOTOR_COUNT; i++) {
     motors[i].run(RELEASE);
   }
   
-  delay(1000);
+  delay(10000);
 }
